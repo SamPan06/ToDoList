ccf9c53ea774217148f53b5e6e444913bd93eeaa
Apple <apple@Apples-MacBook-Pro-101.local>
2019-06-21 15:13:41 -0400

    initial commit

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,20 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Apple on 6/21/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+//   init(listName : String, listImportant : Bool) {
+//        name = listName
+//        important = listImportant
+//    }
+}
+
